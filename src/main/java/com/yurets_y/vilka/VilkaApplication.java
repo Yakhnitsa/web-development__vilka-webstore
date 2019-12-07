6736c03950a8088f3f2be704d346d3e1db0e43f0
Yakhnitsa <transpmailbox@gmail.com>
2019-12-07 21:23:15 +0200

    New project statret

---
+++ src/main/java/com/yurets_y/vilka/VilkaApplication.java
@@ -0,0 +1,13 @@
+package com.yurets_y.vilka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VilkaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VilkaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yurets_y/vilka/VilkaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yurets_y.vilka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VilkaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
